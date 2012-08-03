fefaa41cd1a5c00d9d1749d5dec9cea4ada13b71
Kelly Sutton <michael.k.sutton@gmail.com>
2012-08-03 18:36:23 -0400

    increase jitterPadding

--- jquery.haircut.js
+++ jquery.haircut.js
@@ -3,7 +3,7 @@
     options = {
       bindToResize      : true, //Clips text when the page resizes
       bindToScroll      : true, //Only clips text that's in the viewport
-      jitterPadding     : 10, //The more haircut items on the page, the more it jitters during animation. Adding padding reduces jitter but gives you less usable space.
+      jitterPadding     : 30, //The more haircut items on the page, the more it jitters during animation. Adding padding reduces jitter but gives you less usable space.
       placement         : "middle",
       scrollTimeout     : 50
     },
